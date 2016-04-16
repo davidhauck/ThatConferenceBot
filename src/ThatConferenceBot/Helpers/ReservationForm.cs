2ee3ae6e6e98ff79996248288d66434040fc0a36
awoodward <awoodward@skylinetechnologies.com>
2016-04-16 15:31:10 -0500

    Reservation checking logic

--- src/ThatConferenceBot/Helpers/ReservationForm.cs
+++ src/ThatConferenceBot/Helpers/ReservationForm.cs
@@ -6,12 +6,15 @@ using System.Web;
 using static MyFirstBotApplication.Enums;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Threading.Tasks;
+using MyFirstBotApplication.Domain;
+using MyFirstBotApplication.Domain.Models;
 
 namespace MyFirstBotApplication.Helpers
 {
 	[Serializable]
 	public class ReservationForm
 	{
+       
 		public DateTime? ReservationStartDate { get; set; }
 		public DateTime? EndDate
 		{
@@ -21,7 +24,7 @@ namespace MyFirstBotApplication.Helpers
 			}
 		}
 		public DateTime? ReservationEndDate { get; set; }
-		public Campsite Campsite { get; set; }
+		public MyFirstBotApplication.Enums.Campsite Campsite { get; set; }
 
 		public static IForm<ReservationForm> BuildForm()
 		{
@@ -56,7 +59,41 @@ namespace MyFirstBotApplication.Helpers
 
 		private static Task MakeReservation(IDialogContext context, ReservationForm state)
 		{
-			throw new NotImplementedException();
+            ThatConferenceBotDbContext dbContext = new ThatConferenceBotDbContext();
+
+       //TODO: have to change this to reference Campsite.Id... There is no Id in the enum
+      bool isCampsiteInAReservation = dbContext.Reservations.Where(r => r.Campsite.Name == state.Campsite.ToString()).Any() ;
+            if (isCampsiteInAReservation)
+            {
+                List<Reservation> reservations = dbContext.Reservations.Where(r => r.Campsite.Name == state.Campsite.ToString()).ToList();
+
+                var datesBooked = new List<DateTime>();
+                foreach (Reservation r in reservations)
+                {
+                    for (var dt = r.ArrivalDate; dt <= r.DepartueDate; dt = dt.AddDays(1))
+                    {
+                        datesBooked.Add(dt);
+                    }
+                }
+              
+                var datesRequested = new List<DateTime>();
+
+                for (var date = (DateTime)state.ReservationStartDate; date <= (DateTime)state.ReservationEndDate; date = date.AddDays(1))
+                {
+                    if (date != state.ReservationEndDate)
+                    {
+                        datesRequested.Add(date);
+                    }
+                }
+               
+                foreach (DateTime dt in datesBooked)
+                {
+                    bool isDateBooked  = datesRequested.Contains(dt);
+                }
+             
+
+            }
+            throw new NotImplementedException();
 		}
 	}
 }
