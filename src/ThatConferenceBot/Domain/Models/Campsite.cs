c4b7bbe23eef383553f077ffc5cd16feafcd29b6
awoodward <awoodward@skylinetechnologies.com>
2016-04-16 11:22:15 -0500

    Added Entity framework

---
+++ src/ThatConferenceBot/Domain/Models/Campsite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstBotApplication.Domain.Models
+{
+    public class Campsite
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
