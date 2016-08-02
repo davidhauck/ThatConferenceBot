c0778b4326b8b1967c0dc0660e2c46fed38e32f0
davidhauck <dhauck@LT1114>
2016-08-02 11:54:17 -0500

    Fix luis dialog with form

--- src/ThatConferenceBot/Dialogs/CampingLuisDialog.cs
+++ src/ThatConferenceBot/Dialogs/CampingLuisDialog.cs
@@ -12,37 +12,20 @@ using Microsoft.Bot.Builder.FormFlow;
 
 namespace ThatConferenceBot.Dialogs
 {
-	[LuisModel("fad8bfad-1a75-44c3-a2b8-0d9508486f6f", "c2ffbf490876429f8a88a96b8d2c6ab2")]
+	[LuisModel(Secrets.LuisId, Secrets.LuisKey )]
 	[Serializable]
 	public class CampingLuisDialog : LuisDialog<object>
 	{
-		public const string ENTITY_DATETIME_DATE = "builtin.datetime.date";
-		public override Task StartAsync(IDialogContext context)
-		{
-			return base.StartAsync(context);
-		}
-
-		protected override Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
-		{
-			return base.MessageReceived(context, item);
-		}
-
 		[LuisIntent("Book Campsite")]
 		public async Task BookCampsite(IDialogContext context, LuisResult result)
 		{
-			await context.PostAsync("Creating a campsite reservation. Please type anything to continue.");
-			context.Call(FormDialog.FromForm(ReservationForm.BuildForm), OnResumeFromBookCampsite);
+			context.Call(FormDialog.FromForm(ReservationForm.BuildForm, FormOptions.PromptInStart), OnResumeFromBookCampsite);
 		}
 
 		private async Task OnResumeFromBookCampsite(IDialogContext context, IAwaitable<ReservationForm> result)
 		{
 			await context.PostAsync("What else would you like to do?");
-			context.Done<object>(null);
-		}
-
-		internal static IDialog<ReservationForm> MakeReservationDialog()
-		{
-			return Chain.From(() => FormDialog.FromForm(ReservationForm.BuildForm));
+			context.Wait(MessageReceived);
 		}
 
 		[LuisIntent("None")]
---
+++ src/ThatConferenceBot/Secrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThatConferenceBot
+{
+	public static class Secrets
+	{
+		public const string LuisId = "fad8bfad-1a75-44c3-a2b8-0d9508486f6f";
+		public const string LuisKey = "c2ffbf490876429f8a88a96b8d2c6ab2";
+	}
+}
